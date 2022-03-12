63621f811167362e8ce844fc7d9d9296d6c80bd7
louie_gomez <kinggomez027.lg@gmail.com>
2022-03-12 13:00:37 -0500

    add script.js, connect to index.html

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
-
 const h3 = document.createElement("h3");
-h3.textContent = "Welcome to my website";
+h3.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h3);
+
+const h2 = document.createElement("h2");
+h2.textContent = "Welcome to my website";
+document.querySelector("body").appendChild(h2);
